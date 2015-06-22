4a5b008d616cbf2228575a936939c4c0d4d2d15e
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-22 10:39:25 -0700

    subtract button added, added button modified

--- counter.js
+++ counter.js
@@ -6,8 +6,9 @@ var Counter = Backbone.Model.extend({
 var CounterView = Backbone.View.extend({
     render: function () {
         var val = this.model.get("value");
-        var btn = '<button>Increment</button>';
-        this.$el.html('<p>'+val+'</p>' + btn);
+        var add_btn = '<button class="add">Increment</button>';
+        var subtract_btn = '<button class="subtract">decrement</button>';
+        this.$el.html('<p>'+val+'</p>' + add_btn + subtract_btn);
     }
 });
 
@@ -15,19 +16,25 @@ var counterModel, counterView;
 
 $(document).ready( function () {
 
-	counterModel = new Counter();
+counterModel = new Counter();
 
-	counterView = new CounterView({model : counterModel});
+counterView = new CounterView({model : counterModel});
 counterView.render();
 
 counterModel.on("change", function () {
-    counterView.render();
+	counterView.render();
 });
 
-counterView.$el.on("click","button", function () {
+counterView.$el.on("click",".add", function () {
     var mod = counterView.model;
     var currVal = mod.get("value");
     mod.set("value",currVal+1);
+});
+
+counterView.$el.on("click",".subtract", function () {
+    var mod = counterView.model;
+    var currVal = mod.get("value");
+    mod.set("value",currVal-1);
 });
 
 $("#counterdiv").append(counterView.$el);
