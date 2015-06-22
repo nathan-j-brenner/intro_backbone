afc12ae178b78983f8a2f17721541239d32d627d
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-22 11:30:40 -0700

    separated concatinating from incrementing

--- concatinating_text.js
+++ concatinating_text.js
@@ -1,5 +1,3 @@
-//when the button is clicked, the displayed text is appended to the string from the input
-
 var Counter = Backbone.Model.extend({
 	defaults: {"value": ""}
 })
@@ -10,13 +8,25 @@ var CounterView = Backbone.View.extend({
 		var concat_form = '<form><textarea></textarea></form>'//input text field
 		var concat = '<button class="concat">concatenate</button>'; //button labeled concatenate
 		this.$el.html('<p>' + concat_form + '</p>' + concat + val);
+		// this.$el.html('<p>test</p>');
 
 	}
 });
 
 $(document).ready(function(){
+	var counterModel = new Counter();
+	var counterView = new CounterView({model: counterModel});
+	counterView.render();
 
+	counterModel.on("change", function(){
+		counterView.render();
+	});
+//when the button is clicked, the displayed text is appended to the string from the input
+	counterView.$el.on("click", ".concat", function(){
+		var mod = counterView.model;
+		var currVal = mod.get("value");
+		// mod.set("value,", currVal+)
+	})
 
-
-$("concatdiv").append(counterView.$el);
+$("#concatdiv").append(counterView.$el);
 });
