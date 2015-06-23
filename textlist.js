5aee807e2ffdfdfb3b1503544e70cf5991c8a548
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-23 11:10:11 -0700

    edit increments with keypress or clear button

--- textlist.js
+++ textlist.js
@@ -1,7 +1,10 @@
 $(document).ready( function () {
 
 var TextModel = Backbone.Model.extend({
-    defaults : {"value" : ""},
+    defaults : {
+        "value" : "",
+        "edited": 0 //edited is a value that indicates how many times the text has been cleared or changed (as indicated with the enter keypress);\
+    },
     replace : function (str) {
       this.set("value", str);
     },
@@ -12,7 +15,10 @@ var TextView = Backbone.View.extend({
         var textVal = this.model.get("value");
         var btn = '<button>Clear</button>';
         var input = '<input type="text" value="' + textVal + '" />';
-        this.$el.html(textVal+"<br><div>" + input + btn + "</div>");
+        var edit = this.model.get("edited");
+        var edit_count = '<p>You have edited this ' + edit + ' times.</p>';
+        this.$el.html(textVal+"<br><div>" + input + btn + "<br>" + edit_count +"</div>");
+
     },
     initialize: function () {
         this.model.on("change", this.render, this);
@@ -22,6 +28,7 @@ var TextView = Backbone.View.extend({
     events : {
         "click button" : "clear",
         "keypress input" : "updateOnEnter"
+
     },
     replace : function () {
         var str = this.$el.find("input").val();
@@ -29,11 +36,18 @@ var TextView = Backbone.View.extend({
     },
     clear: function () {
         this.model.replace("");
+        this.edit();
     },
     updateOnEnter: function (e){
         if(e.keyCode == 13) {
             this.replace();
+            this.edit();
         }
+    },
+    edit: function(){ //this function is modeled from the increment function from counter.js
+        var mod = this.model;
+        var currVal = mod.get("edited");
+        mod.set("edited", currVal+1);
     }
 });
 
@@ -42,7 +56,6 @@ var TextCollection = Backbone.Collection.extend({
 });
 
 var TextCollectionView = Backbone.View.extend({
-    // var viewArray = [],
     render : function () {
         //add a delete button to the view of the collection
         var btn = '<button id="addbutton">Add Text</button>';
@@ -88,10 +101,3 @@ textCollectionView.render();
 $("#listdiv").append(textCollectionView.$el);
 
 });
-
-//edited count
-//add a new piece of data to the base model: number of times that it's been edited
-    //every time the field is edited, then the base model should increment
-    //edit: clear or pressed enter
-//modify the view of base model
-//need to modify view of collection?
