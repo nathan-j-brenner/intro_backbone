f188afe48ed3783d5a6daffda48b05b9449a9eb6
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-23 09:08:18 -0700

    started collections exercise

---
+++ textlist.js
@@ -0,0 +1,76 @@
+$(document).ready( function () {
+
+var TextModel = Backbone.Model.extend({
+    defaults : {"value" : ""},
+    replace : function (str) {
+      this.set("value", str);
+    }
+});
+
+var TextView = Backbone.View.extend({
+    render: function () {
+        var textVal = this.model.get("value");
+        var btn = '<button>Clear</button>';
+        var input = '<input type="text" value="' + textVal + '" />';
+        this.$el.html(textVal+"<br><div>" + input + btn + "</div>");
+    },
+    initialize: function () {
+        this.model.on("change", this.render, this);
+        // last argument 'this' ensures that render's
+        // 'this' means the view, not the model
+    },
+    events : {
+        "click button" : "clear",
+        "keypress input" : "updateOnEnter"
+    },
+    replace : function () {
+        var str = this.$el.find("input").val();
+        this.model.replace(str);
+    },
+    clear: function () {
+        this.model.replace("");
+    },
+    updateOnEnter: function (e){
+        if(e.keyCode == 13) {
+            this.replace();
+        }
+    }
+});
+
+var TextCollection = Backbone.Collection.extend({
+    model : TextModel
+});
+
+var TextCollectionView = Backbone.View.extend({
+    render : function () {
+        var btn = '<button id="addbutton">Add Text</button>';
+        var div = '<div id="text-list"></div>';
+        this.$el.html(div + btn);
+    },
+    initialize : function () {
+        this.listenTo(this.collection, 'add', this.addView);
+    },
+    events : {
+        "click #addbutton" : "addModel"
+    },
+    addModel : function () {
+        this.collection.add({});
+        // collection adds a model, fires add event, then listener calls this.addView(model)
+    },
+    addView : function (newModel) {
+        newModel.set("value","Enter something here...");
+        var view = new TextView({model : newModel});
+        view.render();
+        this.$("#text-list").append(view.$el);
+    },
+});
+
+var textCollection = new TextCollection();
+
+var textCollectionView = new TextCollectionView({ collection : textCollection});
+
+textCollectionView.render();
+
+$("#listdiv").append(textCollectionView.$el);
+
+});
